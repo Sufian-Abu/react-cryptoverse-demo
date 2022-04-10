d347998b5058d5c6a5eb8a6cdd4b114072396ebf
Abu Sufian <ynor7005326@gmail.com>
2022-04-11 00:22:52 +0600

    Add Exchange component, add comment, update readme file

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import React from 'react';
+import millify from 'millify';
+import { Collapse, Row, Col, Typography, Avatar } from 'antd';
+import HTMLReactParser from 'html-react-parser';
+
+import { useGetExchangesQuery } from '../services/cryptoApi';
+import Loader from './Loader';
+
+const { Text } = Typography;
+const { Panel } = Collapse;
 
 const Exchanges = () => {
-    return (
-        <div>Exchanges</div>
-    )
-}
+    // This will need premimum subscribtion from RapidAPI
+    const { data, isFetching } = useGetExchangesQuery();
+    const exchangesList = data?.data?.exchanges;
+    // const style = { background: '#0092ff', padding: '8px 0' };
+    if (isFetching) return <Loader />;
 
-export default Exchanges
+    return (
+        <>
+            <Row>
+                <Col span={6}>Exchanges</Col>
+                <Col span={6}>24h Trade Volume</Col>
+                <Col span={6}>Markets</Col>
+                <Col span={6}>Change</Col>
+            </Row>
+            <Row>
+                {exchangesList?.map((exchange) => (
+                    <Col span={24}>
+                        <Collapse>
+                            <Panel
+                                key={exchange.id}
+                                showArrow={false}
+                                header={
+                                    <Row key={exchange.id} className="header-row">
+                                        <Col type="flex" span={6}>
+                                            <Text>
+                                                <strong>{exchange.rank}.</strong>
+                                            </Text>
+                                            <Avatar
+                                                style={{ margin: '0 10px' }}
+                                                src={exchange.iconUrl}
+                                            />
+                                            <Text>
+                                                <strong>{exchange.name}</strong>
+                                            </Text>
+                                        </Col>
+                                        <Col span={6}>${millify(exchange.volume)}</Col>
+                                        <Col span={6}>{millify(exchange.numberOfMarkets)}</Col>
+                                        <Col span={6}>{millify(exchange.marketShare)}%</Col>
+                                    </Row>
+                                }
+                            >
+                                {HTMLReactParser(exchange.description || '')}
+                            </Panel>
+                        </Collapse>
+                    </Col>
+                ))}
+            </Row>
+        </>
+    );
+};
+
+export default Exchanges;
